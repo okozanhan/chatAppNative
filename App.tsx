fad78847d7801cd91bfc8926ffaca1131f10d510
okozanhan <okozanhan01@gmail.com>
2023-07-22 14:26:10 +0300

    deneme

--- App.tsx
+++ App.tsx
@@ -16,8 +16,7 @@
  
  function App(): JSX.Element {
    const isDarkMode = useColorScheme() === 'dark';
- 
-   
+
    const backgroundStyle = {
      backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
    };

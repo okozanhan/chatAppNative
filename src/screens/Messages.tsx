9f7c724a5af70b2530b663a6cec446e7ca8c6348
okozanhan <okozanhan01@gmail.com>
2023-07-23 15:42:09 +0300

    messagesPage css

--- src/screens/Messages.tsx
+++ src/screens/Messages.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Image } from 'react-native'
+import { StyleSheet, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Avatar, Box, Button, Flex, Text, View } from 'native-base';
 import { useNavigation } from '@react-navigation/native';
@@ -13,6 +13,10 @@ const Messages = () => {
 
     const navigation = useNavigation<messagesScreemProp>()
 
+    const redirect = () => {
+        navigation.navigate("Home")
+    }
+
     return (
         <View style={styles.container}>
             <Flex direction='column' style={{ height: '100%' }}>
@@ -21,28 +25,102 @@ const Messages = () => {
                         <Text style={styles.text1} color={'white'}>
                             Welcome User
                         </Text>
-                        <View style={{marginLeft:10}}>
-                            <Icon name="gesture" size={30} color="#900" />
+                        <View style={{ marginLeft: 10 }}>
+                            <Icon name="gesture" size={20} color="#900" />
                         </View>
                     </View>
 
                     <Text style={styles.text2} color={'white'}>
                         ChatApp
                     </Text>
-
-                    <Avatar bg="green.500" style={{ width: 75, height: 75 }} source={{
-                        uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-                    }}>
-                        AJ
-                    </Avatar>
+                    <View style={styles.view2}>
+                        <Text style={styles.story} color={'white'}>
+                            Story
+                        </Text>
+                        <Text style={{ color: "gray" }} color={'white'}>
+                            See All
+                        </Text>
+                    </View>
+                    <View style={styles.view1}>
+                        <View>
+                            <Avatar bg="#1F1B17" style={{ width: 75, height: 75, marginRight: 10 }} >
+                                <Text color={"white"} fontSize={30}>+</Text>
+                            </Avatar>
+                            <Text style={{ color: "gray", fontSize: 13, marginLeft: 10 }}>Add Story</Text>
+                        </View>
+                        <View>
+                            <Avatar bg="green.500" style={{ width: 75, height: 75 }} source={{
+                                uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                            }}>
+                                AJ
+                            </Avatar>
+                            <Text style={{ color: "white" }}>username</Text>
+                        </View>
+                    </View>
                 </Box>
                 <Box style={styles.box2} flex={2} >
-                    <Text color={'black'}>
-                        1skskskskks
-                    </Text>
-                </Box>
+                    <View style={styles.view3}>
+                        <Text fontSize="xl" color={'black'} bold>
+                            Recent Chat
+                        </Text>
+                        <Flex direction='row' alignItems={"center"} backgroundColor={"#F4E0B9"} p={2} borderRadius={50}>
+                            <Icon name="archive" size={20} color="#900" />
+                            <Text color={'black'} bold>
+                                Archive Chat
+                            </Text>
+                        </Flex>
+                    </View>
+                    <Box>
+                        <TouchableOpacity onPress={redirect}>
 
+                            <Flex direction='row' alignItems={"center"} style={styles.message}>
+                                <Avatar bg="green.500" style={{ width: 50, height: 50 }} source={{
+                                    uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                                }}>
+                                    AJ
+                                </Avatar>
+                                <Flex direction='row' justifyContent={"flex-start"} ml={5} style={{ width: "70%" }}>
+                                    <Flex direction='column' alignItems={"flex-start"}>
+                                        <Text color={'black'} bold >
+                                            username
+                                        </Text>
+                                        <Text color={'black'} bold >
+                                            asdasd username username usas
+                                        </Text>
+                                    </Flex>
+                                </Flex>
+                                <Text color={'black'} bold style={{ marginLeft: 10 }}>
+                                    12.12
+                                </Text>
+                            </Flex>
+                        </TouchableOpacity>
+                    </Box>
+                </Box>
+                <View style={{ backgroundColor: "gray", justifyContent: "space-between" }}>
+                    <View style={styles.bottom}>
+                        <View style={styles.tabItem}>
+                            <Icon name="archive" size={20} color="#900" />
+                            <Text color={'white'} >
+                                Calls
+                            </Text>
+                        </View>
+                        <View style={styles.tabItem}>
+                            <Icon name="archive" size={20} color="#900" />
+                            <Text color={'white'} >
+                                Messages
+                            </Text>
+                        </View>
+                        <View style={styles.tabItem}>
+                            <Icon name="archive" size={20} color="#900" />
+                            <Text color={'white'} >
+                                Profile
+                            </Text>
+                        </View>
+                    </View>
+                </View>
             </Flex>
+
+
         </View>
     )
 }
@@ -55,7 +133,23 @@ const styles = StyleSheet.create({
     },
     view1: {
         flexDirection: 'row',
-        alignItems: 'center', 
+        alignItems: 'center',
+    },
+    view2: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: "space-between"
+    },
+    view3: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: "space-between",
+        margin: 25
+    },
+    avatarButton: {
+        width: 50,
+        height: 50,
+        borderRadius: 50
     },
     text1: {
         marginTop: 10,
@@ -63,10 +157,22 @@ const styles = StyleSheet.create({
         color: "gray"
     },
     text2: {
-        marginBottom: 20,
+        marginBottom: 10,
         color: "white",
         fontSize: 30,
         paddingTop: 10
+    },
+    message: {
+        width: "100%",
+        padding: 10,
+    },
+    story: {
+        // marginTop: 10,
+        marginBottom: 10,
+        color: "white",
+        fontSize: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     box1: {
         backgroundColor: "black",
@@ -74,7 +180,22 @@ const styles = StyleSheet.create({
     box2: {
         backgroundColor: "white",
         borderTopStartRadius: 50,
-        borderTopEndRadius: 50
+        borderTopEndRadius: 50,
+    },
+    bottom: {
+        position: 'absolute',
+        bottom: 10,
+        left: 20,
+        right: 20,
+        backgroundColor: '#0D0703',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        padding: 10,
+        borderRadius: 50,
+    },
+    tabItem: {
+        flexDirection: 'column',
+        alignItems: 'center',
     }
 });
 
